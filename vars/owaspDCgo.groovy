89b392377116dd658b3b8646a7505d63b0a47fd9
Ishaan-Dev5 <ishaanaggarwal32@gmail.com>
2025-09-02 13:50:27 +0530

    Update owaspDCgo.groovy

--- vars/owaspDCgo.groovy
+++ vars/owaspDCgo.groovy
@@ -1,1 +1,12 @@
+def call() {
+    echo "Running unit tests in current directory"
 
+    try {
+        // Hardcoded command for unit tests
+        sh 'mvn clean test'
+        echo "Unit tests completed successfully."
+    } catch (err) {
+        echo "Unit tests failed!"
+        error("Stopping pipeline due to failed tests.")
+    }
+}
