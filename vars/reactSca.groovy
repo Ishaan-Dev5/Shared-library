9b15b26631c2f69557574c80fb134e768178caf3
Ishaan-Dev5 <ishaanaggarwal32@gmail.com>
2025-09-04 00:36:00 +0530

    Update reactSca.groovy

--- vars/reactSca.groovy
+++ vars/reactSca.groovy
@@ -1,1 +1,16 @@
+def call(Map config)
+{
+  withSonarQubeEnv('sonarqube'){
+    def scannerHome = tool 'sonar-scanner'
+    sh """
+      ${scannerHome}/bin/sonar-scanner \
+      -Dsonar.projectKey=${config.projectkey} \
+      -Dsonar.projectName=${config.projectName} \
+      -Dsonar.sources=src \
+      -Dsonar.projectVersion=${BUILD_NUMBER}
+    """
+
+    
+  }
+}
 
