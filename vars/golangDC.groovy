4354571e3ae8e75ac2e73065c80583f6154b50da
Ishaan-Dev5 <ishaanaggarwal32@gmail.com>
2025-09-05 20:55:33 +0530

    Create golangDC.groovy

---
+++ vars/golangDC.groovy
@@ -0,0 +1,15 @@
+def call(){
+def goHome = tool name: 'GoLang', type: 'go'
+    env.PATH = "${goHome}/bin:${env.PATH}"
+  try {
+        snykSecurity(
+            snykInstallation: 'snyk_tool',   // Jenkins me configured Snyk installation
+            snykTokenId: 'SNYK_API'          // Jenkins credentials ID
+        )
+    } catch (err) {
+        echo "Snyk scan detected vulnerabilities, but pipeline will continue."
+        echo "Error details: ${err}"
+    }
+}
+
+  
