aba7b28643f3f3a2678eba9b3723a2ffacc5ce65
Ishaan-Dev5 <ishaanaggarwal32@gmail.com>
2025-09-14 19:46:55 +0530

    Update javaunit.groovy

--- vars/javaunit.groovy
+++ vars/javaunit.groovy
@@ -1,17 +1,18 @@
 def call() {
-    // Clone the repo
-    git url: 'https://github.com/Ishaan-Dev5/salary-api.git', branch: 'main'
 
-    echo "Running unit tests"
-    // Run tests but don't fail the pipeline
-    sh 'mvn clean test '
+    runTests()
 
-    echo "Archiving test reports"
-    archiveArtifacts artifacts: 'target/surefire-reports/*.xml', fingerprint: true
+    archiveReports()
+}
 
-    echo "Publishing JUnit reports"
-    // This will mark test results in Jenkins UI but won't fail the pipeline
-    junit allowEmptyResults: true, keepLongStdio: true, testResults: 'target/surefire-reports/*.xml'
 
-    echo "Unit tests completed successfully and reports archived!"
+
+def runTests() {
+    sh 'mvn clean test'
+}
+
+
+def archiveReports() {
+    junit '**/target/surefire-reports/*.xml'
+    archiveArtifacts artifacts: 'target/**/*.jar, target/surefire-reports/*.*', fingerprint: true
 }
